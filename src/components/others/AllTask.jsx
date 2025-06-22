55e9f491816ada6e718ce9aed68dcf1cd4685727
Diksha <dikshadishu0608@gmail.com>
2025-06-22 20:52:44 +0200

    worked with AllTask

--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import {AuthContext} from '../../context/AuthProvider'
+import { AuthContext } from '../../context/AuthProvider'
 
 const AllTask = () => {
 
@@ -8,16 +8,23 @@ const AllTask = () => {
   console.log(authData)
   return (
     <div className='bg-[#1c1c1c] rounded p-4 mt-5 h-40 overflow-auto '>
+      <h2 className='w-1/5 bg-red-600'>Employee Name</h2>
+      <h3 className='w-1/5 bg-red-600'>New Task</h3>
+      <h2 className='w-1/5 bg-red-600'>Active Task</h2>
+      <h3 className='w-1/5 bg-red-600'>Completed</h3>
+      <h3 className='w-1/5 bg-red-600'>Failed</h3>
+      {authData.employees.map(function (elem) {
+        return <div className='bg-red-400 mb-2  py-2 px-4 flex rounded justify-between'>
+          <h2 className='w-1/5 bg-red-600'>{elem.FirstName}</h2>
+          <h3 className='w-1/5 bg-red-600'>Make a UI Design</h3>
+          <h2 className='w-1/5 bg-red-600'>Status</h2>
+          <h3 className='w-1/5 bg-red-600'>Make a UI Design</h3>
+          <h3 className='w-1/5 bg-red-600'>Failed</h3>
 
-      {authData.employees.map(function(elem){
-        return    <div className='bg-red-400 mb-2  py-2 px-4 flex rounded justify-between'>
-            <h2>{elem.FirstName}</h2>
-            <h3>Make a UI Design</h3>
-            <h5>Status</h5>
         </div>
-        
+
       })}
-      
+
     </div>
   )
 }
