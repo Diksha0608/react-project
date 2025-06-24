3430d99322bd5b9b96e58700d2c3fb3f33c77b78
Diksha <dikshadishu0608@gmail.com>
2025-06-24 19:27:24 +0200

    Working on AllTask and createTAsk  component

--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -7,23 +7,23 @@ const AllTask = () => {
   const authData = useContext(AuthContext)
   console.log(authData)
   return (
-    <div className='bg-[#1c1c1c] rounded p-4 mt-5 h-60'>
+    <div className='bg-[#1c1c1c] rounded p-4 mt-5'>
       <div className='bg-red-400 mb-2  py-2 px-4 flex rounded justify-between'>
-          <h2 className='w-1/5 bg-red-600'>EMployee Name</h2>
-          <h3 className='w-1/5 bg-red-600'>New Task</h3>
-          <h2 className='w-1/5 bg-red-600'>Active Task</h2>
-          <h3 className='w-1/5 bg-red-600'>Completed</h3>
-          <h3 className='w-1/5 bg-red-600'>Failed</h3>
+          <h2 className=' text-lg font-medium w-1/5'>EMployee Name</h2>
+          <h3 className=' text-lg font-medium w-1/5'>New Task</h3>
+          <h2 className=' text-lg font-medium w-1/5'>Active Task</h2>
+          <h3 className=' text-lg font-medium w-1/5'>Completed</h3>
+          <h3 className=' text-lg font-medium w-1/5'>Failed</h3>
 
         </div>
- <div className='h-[80%] overflow-auto'>
-       {authData.employees.map(function (elem) {
-        return <div className='bg-red-400 mb-2  py-2 px-4 flex rounded justify-between'>
-          <h2 className='w-1/5 bg-red-600'>{elem.FirstName}</h2>
-          <h3 className='w-1/5 bg-red-600'>Make a UI Design</h3>
-          <h2 className='w-1/5 bg-red-600'>Status</h2>
-          <h3 className='w-1/5 bg-red-600'>Make a UI Design</h3>
-          <h3 className='w-1/5 bg-red-600'>Failed</h3>
+ <div>
+       {authData.employees.map(function (elem,idx) {
+        return <div key={idx} className=' border-2 border-emerald-500 mb-2  py-2 px-4 flex rounded justify-between'>
+          <h2 className=' text-lg font-medium w-1/5'>{elem.FirstName}</h2>
+          <h3 className=' text-lg font-medium w-1/5 !text-blue-600'>{elem.taskCounts.newTask}</h3>
+          <h2 className=' text-lg font-medium w-1/5 !text-yellow-400'>{elem.taskCounts.active}</h2>
+          <h3 className=' text-lg font-medium w-1/5 !text-white'>{elem.taskCounts.completed}</h3>
+          <h3 className=' text-lg font-medium w-1/5 !text-red-600'>{elem.taskCounts.failed}</h3>
 
         </div>
 
--- src/components/others/CreateTask.jsx
+++ src/components/others/CreateTask.jsx
@@ -1,31 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreateTask = () => {
+    const [taskTitle, setTaskTitle] = useState('')
+    const [taskDescription, setTaskDescription] = useState('')
+    const [taskDate, setTaskDate] = useState('')
+    const [assignTo, setAsignTo] = useState('')
+    const [category, setCategory] = useState('')
+
+    const [task, setTask] = useState({})
+
+   const submitHandler = (e) => {
+    e.preventDefault()
+
+setTask({taskTitle,taskDate,taskDescription,category,assignTo,active:false,newTask:true,failed:true,completed:false})
+
+    
+
+   }
+
   return (
       <div className='p-5 bg-[#1c1c1c] mt-7 rounded'>
-                <form className='flex w-full flex-wrap items-start justify-between '>
+                <form onSubmit={(e)=>submitHandler(e)} className='flex w-full flex-wrap items-start justify-between '>
                     <div className='w-1/2'>
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5'>Task Title</h3>
                             <input
+                            value={taskTitle}
+                            onChange={(e)=>setTaskTitle(e.target.value)}
                                 className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4'
                                 type="text" placeholder='make ui design' />
                         </div>
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5'>Date</h3>
                             <input
+                             value={taskDate}
+                            onChange={(e)=>setTaskDate(e.target.value)}
                                 className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4'
                                 type="date" />
                         </div>
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5'>Assign to</h3>
                             <input
+                             value={assignTo}
+                            onChange={(e)=>setAsignTo(e.target.value)}
                                 className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4'
                                 type="text" placeholder='Employee name' />
                         </div>
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5'>Category</h3>
                             <input
+                             value={category}
+                            onChange={(e)=>setCategory(e.target.value)}
                                 className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4'
                                 type="text" placeholder='design, dev, etc' /></div>
                     </div>
@@ -33,6 +58,8 @@ const CreateTask = () => {
                     <div className='w-1/2 flex flex-col items-start'>
                         <h3 className='text-sm text-gray-300 mb-0.5'>Description</h3>
                         <textarea 
+                         value={taskDescription}
+                            onChange={(e)=>setTaskDescription(e.target.value)}
                         className='w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-1 border-gray-400' name="" id="">
                         </textarea>
                         <button className='bg-emerald-500 py-3 hover:bg-amber-600 px-5 rounded text-sm mt-4 w-full'>Create Task</button>
